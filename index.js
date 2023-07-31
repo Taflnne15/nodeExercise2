c8c7b24105d04331ea22d764dbe4706119d395bd
Taflnne15 <taflynnepetersen@gmail.com>
2023-07-31 15:54:21 +0200

    exercise2

---
+++ index.js
@@ -0,0 +1,11 @@
+const Person = require('./person')
+const person1 = new Person('Taflynne', 'Petersen', 'taflynnepetersen@gamil.com')
+console.log(person1);
+const person2 = new Person('Keeno', 'Fourie', 'keenofourie@gmail.com')
+console.log(person2);
+const person3 = new Person('Codi', 'Kader', 'codikader@gmail.com')
+console.log(person3);
+
+
+
+
---
+++ person.js
@@ -0,0 +1,9 @@
+function Person(firstName,lastName, emailAdress){
+this.firstName = firstName,
+this.lastName = lastName,
+this.emailAdress = emailAdress
+    }
+
+
+    module.exports = Person;
+
